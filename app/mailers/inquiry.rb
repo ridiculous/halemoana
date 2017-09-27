622132c267974a04e1d1b77ecbad5790fe772e4a
Ryan Buckley <arebuckley@gmail.com>
2017-09-27 10:00:12 -0700

    Update mailer

--- app/mailers/inquiry.rb
+++ app/mailers/inquiry.rb
@@ -1,10 +1,10 @@
 class Inquiry < ActionMailer::Base
-  default from: 'Tradewinds Cottage <admin@tradewindscottage.net>'
+  default from: 'Hale Moana <admin@halemoana.com>'
 
   def new_inquiry(contact)
     @contact = contact
     @date_range = DateRange.new(@contact.arrival_date, @contact.departure_date)
-    mail(to: 'Brianna <hanagirl143@gmail.com>', subject: 'New inquiry for the Tradewinds Cottage!', bcc: 'Ryan <arebuckley@gmail.com>')
+    mail(to: 'Rebecca <hanamauirentals@gmail.com>', subject: 'New inquiry for Hale Moana', bcc: 'Ryan <arebuckley@gmail.com>')
   end
 
 end
